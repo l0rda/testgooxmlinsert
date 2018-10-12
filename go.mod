3a225f47a5aab31baca0d8ee9081490785906663
K <l0rda@l0rda.biz>
2018-10-12 13:12:54 +0300

    reproduce problem with sheet.InsertRow

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/l0rda/testgooxmlinsert
+
+require baliance.com/gooxml v1.0.0 // indirect
---
+++ main.go
@@ -0,0 +1,24 @@
+package main // import "github.com/l0rda/testgooxmlinsert"
+
+import (
+	"log"
+
+	"baliance.com/gooxml/spreadsheet"
+)
+
+func test1() {
+	wb, err := spreadsheet.Open("test.xlsx")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sheet, err := wb.GetSheet("test")
+	if err != nil {
+		log.Fatal(err)
+	}
+	sheet.InsertRow(2)
+	wb.SaveToFile("out1.xlsx")
+}
+
+func main() {
+	test1()
+}
